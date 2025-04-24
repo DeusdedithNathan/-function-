a984b4aba22511c6e6c14995672dced710667c51
DeusdedithNathan <deusd2070@gmail.com>
2025-04-24 10:25:34 +0300

    ADDING COMMENT

--- PROJECT 2.cpp
+++ PROJECT 2.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 using namespace std;
+// Recursive function for f(n)
 int f(int n) {
     if (n == 1) return 1;  // Base case: f(1) = 1
     return 2 * f(n - 1);   // Recursive step: f(n) = 2 * f(n-1) for n >= 2
+}
