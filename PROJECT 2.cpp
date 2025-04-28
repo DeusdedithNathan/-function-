3e2f77913f86ee64c2e4a24ffd58d09291d9632f
DeusdedithNathan <deusd2070@gmail.com>
2025-04-28 22:01:21 +0300

    continuetion

--- PROJECT 2.cpp
+++ PROJECT 2.cpp
@@ -5,3 +5,7 @@ int f(int n) {
     if (n == 1) return 1;  // Base case: f(1) = 1
     return 2 * f(n - 1);   // Recursive step: f(n) = 2 * f(n-1) for n >= 2
 }
+int main() {
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
